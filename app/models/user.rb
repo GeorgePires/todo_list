14d2ba883762b0635cf6b09dce132fbf30da8ad1
GeorgePires <georgeasp.pi@gmail.com>
2022-11-12 21:22:31 -0300

    :heavy_plus_sign: build/ config devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :tasks
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
+  validates :user_name, presence: true, allow_blank: false
 end
