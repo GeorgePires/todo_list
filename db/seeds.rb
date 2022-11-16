19f6279061db06d44a42bee6b20f86ab087b3b1a
GeorgePires <georgeasp.pi@gmail.com>
2022-11-16 10:19:53 -0300

    :recycle: refactor/ seeds update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
 # Hi!, use the rails db:seed command to create data.
 
+puts "--------> db:seed (Started)"
+
 admin = User.create(email: "admin@gmail.com", password: "123246", password_confirmation: "123246", user_name: "Admin")
 Task.create(description: "Hi!", resolved: false, user_id: admin.id)
+
+puts "--------> db:seed (Successful)"
